b9965332be84369f9e081ababa3a0f6d7527e834
User <User@SYNBLRLT6HZD303.synechron.com>
2021-05-27 15:52:40 +0530

    update jenkinsfile

--- terraform-jenkins-integ/main.tf
+++ terraform-jenkins-integ/main.tf
@@ -1,7 +1,7 @@
-resource "aws_vpc" "javahome_vpc" {
+resource "aws_vpc" "terraform_vpc" {
   cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "${var.tenancy}"
   tags = {
-    Name = "${terraform.workspace}-javahome-vpc"
+    Name = "${terraform.workspace}-terraform-vpc"
   }
 }
